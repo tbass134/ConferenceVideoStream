727026e50c5919ad473e393cc866cb69684e38c9
Tony Hung <tbass134@gmail.com>
2016-09-27 21:17:14 -0400

    aws

--- ConferenceStreaming/ServiceManager.swift
+++ ConferenceStreaming/ServiceManager.swift
@@ -25,18 +25,46 @@ class ServiceManager {
         let enUSPosixLocale = NSLocale(localeIdentifier: "en_US_POSIX")
         formatter.locale = enUSPosixLocale
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+
         return formatter
     }()
     
     private init() {
         
-       
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.USEast1,
+                                                                identityPoolId:"us-east-1:473cd8fa-777e-4d74-926d-ea4438e7d92a")
+        
+        let configuration = AWSServiceConfiguration(region:.USEast1, credentialsProvider:credentialsProvider)
+        
+        AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = configuration
+
     }
     
     func loadItems(operation:String = "read", id:String = "*", completionBlock:CompletionBlock) {
         
-           }
+        let lambdaInvoker = AWSLambdaInvoker.defaultLambdaInvoker()
+        let params = ["operation":operation, "id":id]
+        lambdaInvoker.invokeFunction("ConferenceVideoBackend", JSONObject: params) { (results, error) in
+            
+            var list = [Video]()
+            
+            if let videos = results as? Array<Dictionary<String, String>>{
+                for data in videos {
+                   list.append(Video(videoID: data["id"], title: data["title"]!, date: NSDate.sam_dateFromISO8601String(data["date"]!), url: NSURL(string: data["url"]!)!))
+                }
+            }
+            print(list)
+            completionBlock(results: results, error: error)
+            
+        }
+    }
     
     func addVideo(video:Video, completionBlock:CompletionBlock) {
-         }
+        let lambdaInvoker = AWSLambdaInvoker.defaultLambdaInvoker()
+        let params = ["operation":"create", "title":video.title, "url":video.url.absoluteString]
+        lambdaInvoker.invokeFunction("ConferenceVideoBackend", JSONObject: params) { (results, error) in
+            completionBlock(results: results, error: error)
+            
+        }
+    }
 }
